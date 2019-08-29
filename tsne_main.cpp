ee9b5cb5c03b13da5b0dc926b149147d76ed5bc8
wdm <wdm@wdm>
2019-08-30 00:34:42 +0700

    new tsne test(not completed)

--- tsne_main.cpp
+++ tsne_main.cpp
@@ -15,6 +15,9 @@ int main() {
     int rand_seed = -1;
 
     // Read the parameters and the dataset
+
+    // выделяет память под массив данных как D*N*sizeof(double) - D оригинальная размерность
+
 	if(TSNE::load_data(&data, &origN, &D, &no_dims, &theta, &perplexity, &rand_seed, &max_iter)) {
 
 		// Make dummy landmarks
@@ -27,6 +30,22 @@ int main() {
 		double* Y = (double*) malloc(N * no_dims * sizeof(double));
 		double* costs = (double*) calloc(N, sizeof(double));
         if(Y == NULL || costs == NULL) { printf("Memory allocation failed!\n"); exit(1); }
+        // perplexity - В данном случае перплексия может быть интерпретирована как сглаженная оценка эффективного
+        // количества «соседей» для точки. from 5 to 50
+
+        //data - данные
+
+        // N - длинна массива данных
+
+        // Y - массив результатов
+
+        // D - видимо исходная размерность данных
+
+        // no_dims - требуемая размерность (2)
+
+        // theta - шаг обучения
+
+        // max_iter - 1000
 		TSNE::run(data, N, D, Y, no_dims, perplexity, theta, rand_seed, false, max_iter, 250, 250);
 
 		// Save the results
