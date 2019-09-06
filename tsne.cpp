ca2263a007fff2f43bfb1401af5b9bb4f660a3c7
wdm <wdm@wdm>
2019-09-06 14:39:09 +0700

    fix some error and added new

--- tsne.cpp
+++ tsne.cpp
@@ -619,7 +619,8 @@ static void computeSquaredEuclideanDistance(double* X, int N, int D, double* DD)
         double* curr_elem = &DD[n*N + n];
         *curr_elem = 0.0;
         double* curr_elem_sym = curr_elem + N;
-        for(int m = n + 1; m < N; ++m, XmD+=D, curr_elem_sym+=N) {
+        *curr_elem_sym = 0;
+        for(int m = n + 1; m < N; ++m, XmD += D, curr_elem_sym += N) {
             *(++curr_elem) = 0.0;
             for(int d = 0; d < D; ++d) {
                 *curr_elem += (XnD[d] - XmD[d]) * (XnD[d] - XmD[d]);
